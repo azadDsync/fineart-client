352a27675c8e40b1f034f3610c7c0cf3e8223a04
Domendra Singh Komra <142248999+azadDsync@users.noreply.github.com>
2025-10-07 19:57:52 +0530

    Change social sign-in callbackURL to profile
    Update callbackURL for social sign-in to point to profile.

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -90,7 +90,7 @@ export default function SignInForm() {
     try {
       await authClient.signIn.social({
         provider,
-        callbackURL: `${process.env.NEXT_PUBLIC_CLIENT_URL}/dashboard`,
+        // callbackURL: `${process.env.NEXT_PUBLIC_CLIENT_URL}/profile`,
       });
     } catch (error: unknown) {
       console.log(error);
