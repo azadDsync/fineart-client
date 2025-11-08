dc411138650273493c1d2981b6b42e45eab91261
azadDsync <domendrasinghkomra750@gmail.com>
2025-11-09 02:57:00 +0530

    resolved naming issue for .webmanifest and removed non std entries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,6 @@ export const metadata: Metadata = {
       { url: "/favicon_io/favicon.ico" },
     ],
     apple: "/favicon_io/apple-touch-icon.png",
-    other: [
-      { rel: "android-chrome-192x192", url: "/favicon_io/android-chrome-192x192.png" },
-      { rel: "android-chrome-512x512", url: "/favicon_io/android-chrome-512x512.png" },
-    ],
   },
   manifest: "/favicon_io/site.webmanifest",
 };
